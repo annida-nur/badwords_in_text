cf44b55f9da1ba7c6608096c158858ff22a974f3
Annida Nur Islami <91865778+annida-nur@users.noreply.github.com>
2024-02-09 12:45:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,29 +13,35 @@ except :
     from BeautifulSoup import BeautifulSoup 
 import logging
 import shutil
+
 def inject_ga():
-    GA_ID = "G-3XHJ5EL5Q5"
+    GA_ID = "google_analytics"
     GA_JS = """
-    <script async src = "https://www.googletagmanager.com/gtag/js?=id=G-3XHJ5EL5Q5"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3XHJ5EL5Q5"></script>
     <script>
-        window.datalayer = window.dataLayer || [];
+        window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config','G-3XHJ5EL5Q5');
+
+        gtag('config', 'G-3XHJ5EL5Q5');
     </script>
     """
 
+    # Insert the script in the head tag of the static template inside your virtual
     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID):
+    if not soup.find(id=GA_ID): 
         bck_index = index_path.with_suffix('.bck')
         if bck_index.exists():
-            shutil.copy(bck_index, index_path)
+            shutil.copy(bck_index, index_path)  
         else:
-            shutil.copy(index_path, bck_index)
+            shutil.copy(index_path, bck_index)  
         html = str(soup)
-        new_html = html.replace('<head>','<head>\n' + GA_JS)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
         index_path.write_text(new_html)
+
 
 inject_ga()
 ##########################################################################
