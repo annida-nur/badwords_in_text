dff826a15a901749bde247858d6c5d68cbff69d9
Annida Nur Islami <91865778+annida-nur@users.noreply.github.com>
2024-02-09 13:25:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,10 +40,9 @@ def inject_ga():
     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
     logging.info(f'editing {index_path}')
     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID):  # if cannot find tag
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_JS)
-        index_path.write_text(new_html)
+    html = str(soup)
+    new_html = html.replace('<head>', '<head>\n' + GA_JS)
+    index_path.write_text(new_html)
 
 
 inject_ga()
