667d2bdd101deccca10bd5134f62be56d1e2e839
Annida Nur Islami <91865778+annida-nur@users.noreply.github.com>
2024-02-09 13:07:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,34 +19,39 @@ import pathlib
 import shutil
 import streamlit as st
 
-GA_ID = "google_analytics"
-GA_SCRIPT = """
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-3XHJ5EL5Q5"></script>
-<script id='google_analytics'>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-3XHJ5EL5Q5');
-</script>
-"""
-
 def inject_ga():
+    GA_ID = "google_analytics"
+
+    # Note: Please replace the id from G-XXXXXXXXXX to whatever your
+    # web application's id is. You will find this in your Google Analytics account
     
+    GA_JS = """
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3XHJ5EL5Q5"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-3XHJ5EL5Q5');
+    </script>
+    """
+
+    # Insert the script in the head tag of the static template inside your virtual
     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID): 
+    if not soup.find(id=GA_ID):  # if cannot find tag
         bck_index = index_path.with_suffix('.bck')
         if bck_index.exists():
-            shutil.copy(bck_index, index_path)
+            shutil.copy(bck_index, index_path)  # recover from backup
         else:
-            shutil.copy(index_path, bck_index)  
+            shutil.copy(index_path, bck_index)  # keep a backup
         html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
         index_path.write_text(new_html)
 
-inject_ga()
-##########################################################################
+
+inject_ga()##########################################################################
 #function
 def clean(text):
     text = re.sub('[^a-zA-Z]+', ' ', text)
