d9f142d3208dc1d2952e43bd0a5e5aad689d5b39
Annida Nur Islami <91865778+annida-nur@users.noreply.github.com>
2024-02-09 13:19:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,11 +41,12 @@ def inject_ga():
     logging.info(f'editing {index_path}')
     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
     if not soup.find(id=GA_ID):  # if cannot find tag
-        bck_index = index_path.with_suffix('.bck')
+        bck_index = index_path
         if bck_index.exists():
             shutil.copy(bck_index, index_path)  # recover from backup
         else:
             shutil.copy(index_path, bck_index)  # keep a backup
+        bck_index = bck_index.with_suffix(".bck")
         html = str(soup)
         new_html = html.replace('<head>', '<head>\n' + GA_JS)
         index_path.write_text(new_html)
